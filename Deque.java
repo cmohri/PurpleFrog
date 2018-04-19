8ff4b2dac842fcb7e9686f9fa8bea3be2846a96f
Clara Mohri <claramohri@napoleon.local>
2018-04-19 13:32:26 -0400

    update

--- Deque.java
+++ Deque.java
@@ -1,14 +1,18 @@
 public interface Deque<T>{
 
+    //returns value of first or last, without removing from the queue
     public DLLNode<T> peekFirst();
     public DLLNode<T> peekLast();
     
+    //adds to the front or the end
     public void addFirst(T x);
     public void addLast(T x);
     
+    //removes from front or end
     public T removeFirst();
     public T removeLast();
 
+    //returns size of the queue
     public int size();
 
 }
--- QQKachoo.java
+++ QQKachoo.java
@@ -1,3 +1,51 @@
-public class QQKachoo<D> implements Deque<D>{
+public class QQKachoo<T> implements Deque<T>{
 
+    private DLLNode<T> _front;
+    private DLLNode<T> _end;
+    private int _size;
+
+    public QQKachoo(){
+	_front = null;
+	_end = null;
+    }
+
+    public T peekFirst(){
+	return _front.getValue();
+    }
+
+    public T peekLast(){
+	return _end.getValue();
+    }
+
+    public T removeFirst(){
+	T retVal = peekFirst();
+	_front = _front.getNext();
+	return retVal;
+	_size -=1 ;
+    }
+
+    public T removeLast(){
+	T retVal = peekLast();
+	_last = _last.getLast();
+	_size -= 1;
+	return retVal;
+    }
+
+    public int size(){
+	return _size;
+    }
+
+    public boolean isEmpty(){
+	return _size <= 0;
+    }
+
+    public void addFirst(T x){
+	DLLNode<T> node = new DLLNode<T>(x, null, _front);
+	_front = node;
+    }
+    
+    public void addLast(T x){
+	DLLNode<T> node = new DLLNode<T>(x, _end, null);
+	_end = node;
+    }
 }
